a6380782d3ddca78867c4b311eac117b9de56ce2
Kyle Barnett <kbarnett23@gmail.com>
2018-11-13 12:13:13 -0700

    userform

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,15 +1,67 @@
 import React, { Component } from 'react';
+import FormUserDetails from './FormUserDetails';
 
 class UserForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      step: 1,
+      firstName: '',
+      lastName: '',
+      email: '',
+      occupation: '',
+      city: '',
+      bio: ''
+    }
+  }
+  //proceed to next step
+  nextStep = () => {
+    const {step} = this.state
+    this.setState({
+      step: step + 1
+    })
+  }
+  //go back to previous step
+  prevStep = () => {
+    const {step} = this.state
+    this.setState({
+      step: step - 1
+    })
+  }
+
+  //handled fields change
+  handleChange = input => e => {
+    this.setState({
+      [input]: e.targer.value
+    })
   }
   render() {
-    return (
-      <div>
-      </div>
-    );
+    const {step} = this.state
+    const {firstName, lastName, email, occupation, city, bio} = this.state
+    const values = {firstName, lastName, email, occupation, city, bio}
+
+    switch(step){
+      case 1: 
+        return (
+          <FormUserDetails 
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        )
+      case 2:
+        return (
+          <h1>FormPersonalDetails</h1>
+        )
+      case 3:
+        return (
+          <h1>Confirm</h1>
+        )
+      case 4:
+        return (
+          <h1>Success</h1>
+        )
+    }
   }
 }
 
